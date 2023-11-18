efa42130c290e6fea1faf7d8136031ee92bb8d50
TiboMertens <tibomertens25@gmail.com>
2023-11-18 14:36:46 +0100

    chore: add start files

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Chat from './components/chat/chat.vue';
+import VideoDetails from './components/video/VideoDetails.vue';
+import VideoPlayer from './components/video/videoPlayer.vue';
+
+import { ref } from 'vue';
+
+let newDescription = ref("Default description");
+
+const updateVideo = (description) => {
+  newDescription.value = description;
+};
+
+</script>
+
+<template>
+  <div class="tiktok">
+    <div>
+      <VideoPlayer @update:video-description="updateVideo" />
+    </div>
+    <div>
+      <VideoDetails :description="newDescription" />
+      <Chat />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .tiktok {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+</style>
---
+++ src/components/chat/Chat.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, reactive, onMounted } from 'vue';
+let message = ref("JUUUUUWWW"); //voor int, string, boolean
+let messages = reactive({
+  data: ["heel", "even", "testen"]
+}); //voor objecten en arrays
+
+const sendMessage = () => {
+  messages.data.push(message.value);
+  message.value = "";
+};
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="m in messages.data">{{ m }}</li>
+    </ul>
+  </div>
+  <div>
+    <input v-model="message" type="text" placeholder="">
+    <button @click="sendMessage">Send</button>
+  </div>
+</template>
+
+<style scoped></style>
